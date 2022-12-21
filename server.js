61dc2f4aed987ac26ad47ab039c974ed0fc41d7d
Laila Hamed Naseeb (ليلى حامد نصيب) <LHamed@mohe.edu.kw>
2022-12-21 13:41:04 +0300

    Connecting to database with Mongoose

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 
-// Route files
-const bootcamps = require('./routes/bootcamps.js')
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps.js')
 
 const app = express();
 
@@ -22,4 +27,12 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection',(err, promise)=>{
+    console.log(`Unhandled Rejection (Error): ${err.message}`);
+
+    // Close server and exit process with failure (1)
+    server.close(()=>process.exit(1));
+})
