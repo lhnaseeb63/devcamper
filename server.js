ec4ef26c58966a3b54fb5b221599a71db98602b4
Laila Hamed Naseeb (ليلى حامد نصيب) <LHamed@mohe.edu.kw>
2022-12-21 10:04:41 +0300

    had to remake the config folder

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+const loggeer = (req, res, next) => {
+    req.hello = "Hello World 2";
+    console.log('Middleware has run run run');
+    next();
+}
+
+app.use(loggeer);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
