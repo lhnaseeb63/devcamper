fc48cb2cec66b779eb18ae30d77706479e2955c7
Laila Hamed Naseeb (ليلى حامد نصيب) <LHamed@mohe.edu.kw>
2022-12-21 11:10:03 +0300

    Creating Routes and Responses in Express

--- server.js
+++ server.js
@@ -6,13 +6,34 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-const loggeer = (req, res, next) => {
+const logger = (req, res, next) => {
     req.hello = "Hello World 2";
     console.log('Middleware has run run run');
     next();
 }
 
-app.use(loggeer);
+app.use(logger);
+
+app.get('/api/v1/bootcamps', (req, res)=> {
+    res.status(200).json({success:true, msg:'Show all bootcamps'});
+});
+
+app.get('/api/v1/bootcamps/:id', (req, res)=> {
+    res.status(200).json({success:true, msg:`Get single bootcamp ${req.params.id}`});
+});
+
+app.post('/api/v1/bootcamps', (req, res)=> {
+    res.status(200).json({success:true, msg:'Create new bootcamp'});
+});
+
+app.put('/api/v1/bootcamps/:id', (req, res)=> {
+    res.status(200).json({success:true, msg:`Update bootcamp ${req.params.id}`});
+});
+
+app.delete('/api/v1/bootcamps/:id', (req, res)=> {
+    res.status(200).json({success:true, msg:`Delete single bootcamp ${req.params.id}`});
+});
+
 
 const PORT = process.env.PORT || 5000;
 
